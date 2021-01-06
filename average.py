2acd3cf2e0457f4e2f31c3c717d4f69bf2996373
wls4421 <wls4421@naver.com>
2021-01-07 01:04:11 +0900

    average

---
+++ average.py
@@ -0,0 +1,21 @@
+testcase = int(input())
+addresult = 0
+avgresult = []
+count = 0
+percent = []
+array = [[0 for col in range(testcase)]for row in range(1000)]
+for i in range(testcase) :
+      array[i] = list(map(int, input().split()))
+      for k in range(0,array[i][0]+1):
+        if k == 0 : addresult = 0 
+        else : addresult = addresult + array[i][k]
+      avgresult.append(addresult/array[i][0])
+      addresult = 0
+      for j in range(0,array[i][0]+1):
+          if array[i][j] > avgresult[i] : count = count + 1
+          else : count = count
+      percent.append(count/array[i][0])
+      count = 0
+
+for j in range(testcase) : 
+    print(round(percent[j]*100,3),'%')
