a488ca0e59aac635e1b63232549f3b749785a74d
wls4421 <wls4421@naver.com>
2021-02-01 17:14:30 +0900

    router

---
+++ router.py
@@ -0,0 +1,32 @@
+from sys import stdin
+import math
+
+Hnum, R = stdin.readline().split()
+Hnum = int(Hnum)
+R = int(R)
+H = []
+for i in range(Hnum):
+    H.append(int(stdin.readline()))
+
+H.sort()
+
+total = H[-1] - H[0]
+gap = 1
+result = 0
+
+while(gap <= total):
+    median = math.trunc((gap + total) / 2)
+    count = 1
+    start = H[0]
+    for i in range(1, len(H)):
+        if H[i] >= start + median:
+            start = H[i]
+            count += 1
+    if count >= R:
+        gap = median + 1
+        result = median
+    else:
+        total = median -1
+
+print(result)
+
